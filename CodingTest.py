60311df360f8ca5be567673701f4847d6b5d0f06
SUNGIL KIM <sonny0204@gmail.com>
2024-02-15 15:55:09 +0900

    First Python commit

---
+++ CodingTest.py
@@ -0,0 +1,12 @@
+def solution(stringData):
+    answer = ""
+    if(len(stringData) % 2 == 0):
+        num = len(stringData) // 2 
+        answer = stringData[num-1:num+1]
+        return answer
+    
+    num = len(stringData)//2
+    answer = stringData[num]
+    return answer 
+
+print(solution("qwer"))
