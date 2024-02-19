aef172b2e1db8fbe39415e6ad6bb778722a94048
SUNGIL KIM <sonny0204@gmail.com>
2024-02-19 14:27:53 +0900

    올바른괄호 프로그래머스 Level 1

---
+++ corretParent.py
@@ -0,0 +1,21 @@
+def solution(s):
+    answer = True
+    list = [] 
+    for i in s:
+        if i =="(":
+            list.append(1)
+        else :
+            if not list :
+                return False 
+            list.pop()
+    if len(list) == 0:
+        answer= True 
+    else :
+        answer = False 
+    print(answer) 
+    return answer 
+
+# solution("()()")
+# solution("(())()")
+solution(")()(")
+solution("(()(")
