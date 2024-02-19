1ef1ca62b1cf0808f8c58f2bd3ab4d8da33e9ad9
SUNGIL KIM <sonny0204@gmail.com>
2024-02-19 16:59:29 +0900

    체육복 프로그래머스 Level1

--- gymSuit.py
+++ gymSuit.py
@@ -1,18 +1,16 @@
 def solution(n , lost , reserve):
-    answer = 0 
     new_lost = set(lost) - set(reserve)
     new_reserve = set(reserve) - set(lost)
 
     for i in new_lost :
         if i + 1 in new_reserve :
             new_reserve.remove(i+1)
-        if i -1 in new_reserve :
+        elif i -1 in new_reserve:
             new_reserve.remove(i-1)
         else :
-            n-1
+          n-=1
     print(n)
     return n 
-
 solution(5 , [2,4] ,[1,3,5])
 solution(5 , [2,4] ,[3])
-solution(5 , [3] ,[1])
+solution(3 , [3] ,[1])
